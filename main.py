16fe993f93ee65c537e6fc5b33a634756d134d74
Drages <richard001122@126.com>
2026-01-15 14:27:19 +0800

    fix: 添加多进程支持以防止Windows平台下的冻结问题
    添加multiprocessing.freeze_support()调用以解决在Windows平台打包为可执行文件时的多进程问题

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import multiprocessing
 import tkinter as tk
 
 from src.gui import DigitRecognitionApp
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     root = tk.Tk()
     app = DigitRecognitionApp(root)
     root.mainloop()
