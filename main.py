4c2a0f5c41da43bb359fda0b6c7fb10ed46650e4
Drages <richard001122@126.com>
2026-01-26 18:51:14 +0800

    start refractor

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import multiprocessing
 import tkinter as tk
+from tkinter import Tk
 
 from src.gui import DigitRecognitionApp
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    root = tk.Tk()
-    app = DigitRecognitionApp(root)
+    root: Tk = tk.Tk()
+    app: DigitRecognitionApp = DigitRecognitionApp(root)
     root.mainloop()
